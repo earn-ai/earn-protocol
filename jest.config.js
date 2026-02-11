112b2403a3ce78acb9575dcd34854f9830df1893
earn-ai <moltearn@proton.me>
2026-02-11 10:53:37 +0000

    fix: simplify jest config, remove preset for better compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      useESM: true,
-    }],
+    '^.+\\.ts$': 'ts-jest',
   },
   collectCoverageFrom: ['src/**/*.ts'],
   testTimeout: 30000,
